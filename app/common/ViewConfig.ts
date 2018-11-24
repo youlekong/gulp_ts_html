adb5933e49542e8473bdece8ee714b1dfa29bc91
张超凡 <313008430@qq.com>
2018-11-24 18:41:28 +0800

    view.....

--- app/common/ViewConfig.ts
+++ app/common/ViewConfig.ts
@@ -4,6 +4,13 @@ import GameLogic from "../logic/view_logic/GameLogic";
 import FindLogic from "../logic/view_logic/FindLogic";
 import NewsContent from "../logic/view_logic/NewsContent";
 import CollectLogic from "../logic/view_logic/CollectLogic";
+import RecahrgeLogic from "../logic/view_logic/RechargeLogic";
+import RecahrgeSuccess from "../logic/view_logic/RechargeSuccess";
+import RecRcods from "../logic/view_logic/RecRcods";
+import IntegralEx from "../logic/view_logic/IntegralEx";
+import OrderDetail from "../logic/view_logic/OrderDetail";
+import IntegralDetail from "../logic/view_logic/IntegralDetail";
+import AwardsBox from "../logic/view_logic/AwardsBox";
 
 /**
  * 界面配置，路径，对应的类等等
@@ -21,4 +28,18 @@ export default class ViewConfig {
     static game: viewConfig = { name: 'game', class: GameLogic, skin: 'view/game.html', closePre: false  };
     /**测试页 */
     static alert: viewConfig = { name: 'alert', class: AlertLogic, skin: 'view/alert.html', closePre: true  };
+     /**充值 */
+     static recharge: viewConfig = { name: 'recharge', class: RecahrgeLogic, skin: 'view/recharge.html', closePre: true  };
+      /**充值成功 */
+      static rechargeSuccess: viewConfig = { name: 'rechargeSuccess', class: RecahrgeSuccess, skin: 'view/rechargeSuccess.html', closePre: true  };
+      /**充值记录 */
+      static recRcods: viewConfig = { name: 'recRcods', class: RecRcods, skin: 'view/recRcods.html', closePre: true  };
+      /**积分兑 */
+      static integralex: viewConfig = { name: 'integralex', class: IntegralEx, skin: 'view/integralex.html', closePre: true  };
+      /**订单详细 */
+      static orderdetail: viewConfig = { name: 'orderdetail', class: OrderDetail, skin: 'view/orderdetail.html', closePre: true  };
+      /**积分兑详细 */
+      static integraldetail: viewConfig = { name: 'integraldetail', class: IntegralDetail, skin: 'view/integraldetail.html', closePre: true  };
+      /**奖品柜 */
+      static awardsbox: viewConfig = { name: 'awardsbox', class: AwardsBox, skin: 'view/awardsbox.html', closePre: true  };
 }
---
+++ app/logic/view_logic/RechargeSuccess.ts
@@ -0,0 +1,14 @@
+import ViewBase from "../../core/ViewBase";
+
+
+
+export default class RecahrgeSuccess extends ViewBase {
+
+    onEnable(){
+        // Core.viewManager.closeView(Core.preView);
+    }
+
+    onClick(e) {
+        console.log(e)
+    }
+} 

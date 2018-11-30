ba691eb14fc50ce681f711c10f811f37e4d3d69a
张超凡 <313008430@qq.com>
2018-11-30 13:37:02 +0800

    第三关内页

--- app/common/Config.ts
+++ app/common/Config.ts
@@ -3,7 +3,7 @@
  */
 export default class Config {
     /**数据域名 */
-    static baseUrl:string = '//api.khj.com/';
+    static baseUrl:string = 'http://api-h5.786g.com/';
     /**图片域名 */
     static imgBase:string = 'https://img.okwan.com/'
 }

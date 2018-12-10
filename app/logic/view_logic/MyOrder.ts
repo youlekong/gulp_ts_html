e788df77adaa4199985848f73f5d5eac2c820fe2
LIXIAOWEN\Administrator <1336429771@qq.com>
2018-12-10 20:27:19 +0800

    我的订单

--- app/logic/view_logic/MyOrder.ts
+++ app/logic/view_logic/MyOrder.ts
@@ -24,9 +24,53 @@ export default class MyOrder extends ViewBase {
         })
 
         let OrderList = await Net.getData(Api.OrderList);
-        console.log(OrderList)
+        let awardsBox = await Net.getData(Api.awardsBox);
+        this.setOrderList(OrderList,awardsBox);
+
+       
+
+    }
 
 
+    /**
+     * 订单列表
+     * @param OrderList  订单列表
+     * @param awardsBox  奖品柜列表
+     */
+
+    private setOrderList(OrderList: any,awardsBox: any){
+        let html='';
+        let htm1='';
+        for(let x=0;x<OrderList.length;x++){         
+            html+=`<li>
+                    <div class="top">
+                        <p class="time">${OrderList[x]['time']}</p>
+                    </div>
+                    <div class="con">
+                        <div class="list"></div>
+                        <div class="bottom">
+                            <span>共计${OrderList[x]['goodLists'].length}件商品</span>
+                        </div>
+                    </div>
+                </li>`
+            for(let y=0;y<OrderList[y]['goodLists'].length;y++){
+                let goodLists=OrderList[y]['goodLists'];
+                if (awardsBox[y]['id'] == goodLists[y]['user_goods_id']) {
+                    htm1+=`<div class="infot">
+                            <div class="pic"><img src="${Config.imgBase +awardsBox[y]['src']}" /></div>
+                            <div class="info">
+                                <p class="t">${awardsBox[y]['title']}</p>
+                            </div>
+                        </div>`
+                }
+            }
+            
+        }
+        $("#orderList").html(html);
+        $("#orderList li").forEach(function(item){
+            $(item).find(".list").append(htm1);
+        })
+        
     }
 
     onClick(e) {
